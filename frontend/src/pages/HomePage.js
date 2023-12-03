6b92cbe720c13cf935bc7d0d021c867a3d05befb
Harold-coder <hjc2154@columbia.edu>
2023-12-02 22:45:32 -0500

    Connection to smart contract works. Only rendering doctorPage if a doctor

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,30 +1,55 @@
-import React from 'react';
-import { useState } from 'react';
+import Web3 from 'web3';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom'; // Import useNavigate from react-router-dom
 import '../styles/HomePage.css';
+import DoctorVerificationABI from './contractsABI';
 
 const HomePage = () => {
-    const [walletAddress, setWalletAddress] = useState('');
+    const [isDoctor, setIsDoctor] = useState(null); // State to keep track of doctor verification
+    const [errorMessage, setErrorMessage] = useState(''); // State to store error message
+    const navigate = useNavigate(); // Hook to navigate programmatically
 
-    const connectWallet = async () => {
-        if (window.ethereum) {
-            try {
-                const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-                setWalletAddress(accounts[0]);
-                console.log(walletAddress);
-            } catch (error) {
-                console.error("Failed to connect wallet", error);
-            }
-        } else {
-            alert('Please install MetaMask to use this feature!');
-        }
+    // Initialize web3
+    const web3 = new Web3(Web3.givenProvider || "http://localhost:8545");
+
+    // Instantiate the contract
+    const contractABI = DoctorVerificationABI;
+    const doctorVerificationContractAddress = "0x96E8ae694dfCa02e6d7e27434CF0e732221C6C63";
+    const doctorVerificationContract = new web3.eth.Contract(contractABI, doctorVerificationContractAddress);
+
+    // Function to request access to the user's wallet and get the account
+    const getWalletAddress = async () => {
+        const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+        return accounts[0];
     };
 
+    // Function to check if the connected wallet address is a doctor
+    async function checkIfDoctor(walletAddress) {
+        try {
+            const isDoctor = await doctorVerificationContract.methods.verifyDoctor(walletAddress).call();
+            setIsDoctor(isDoctor); // Set the verification result
+            if (isDoctor) {
+                navigate('/doctor'); // Navigate to DoctorPage if the user is a verified doctor
+            } else {
+                setErrorMessage('You are not recognized as a doctor. Please contact us to be added to the list.'); // Set error message
+            }
+        } catch (error) {
+            console.error("An error occurred while verifying the doctor:", error);
+            setErrorMessage('There was an error verifying your doctor status.'); // Set error message for other errors
+        }
+    }
+
+    async function verifyDoctor() {
+        const account = await getWalletAddress();
+        await checkIfDoctor(account);
+    }
+    
     return (
         <div className="home">
-            <nav className="navbar">
+             <nav className="navbar">
                 <span className="title">AuroraChain</span>
-                <button onClick={connectWallet} className="connect-wallet-btn">
+                <button onClick={getWalletAddress} className="connect-wallet-btn">
                     Connect Wallet
                 </button>
             </nav>
@@ -41,10 +66,11 @@ const HomePage = () => {
             <div className="role-selection">
                 <h1>I am a...</h1>
                 <div className="role-buttons">
-                    <Link to="/doctor" className="role-btn">Doctor</Link>
+                    <button className='role-btn' onClick={verifyDoctor}>Doctor</button>
                     <Link to="/patient" className="role-btn">Patient</Link>
                     <Link to="/pharmacist" className="role-btn">Pharmacist</Link>
                 </div>
+                {errorMessage && <p className="error-message">{errorMessage}</p>}
             </div>
         </div>
     );
---
+++ frontend/src/pages/contractsABI.js
@@ -0,0 +1,87 @@
+const DoctorVerificationABI = [
+	{
+		"inputs": [],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_doctorAddress",
+				"type": "address"
+			}
+		],
+		"name": "addDoctor",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"name": "isDoctor",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "owner",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_doctorAddress",
+				"type": "address"
+			}
+		],
+		"name": "removeDoctor",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_doctorAddress",
+				"type": "address"
+			}
+		],
+		"name": "verifyDoctor",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+];
+  
+  export default DoctorVerificationABI;
+  
